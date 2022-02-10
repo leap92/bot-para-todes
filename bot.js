ade4fdca2a2baa31087d5cfbeb7058375ea1d305
Leandro Pross <leacap10@gmail.com>
2022-02-10 20:49:00 -0300

    use includes

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ bot.on('messageCreate', async (msg) => {
 
     if (botWasMentioned) {
         try {
-            const leaderboard_id = msg.content === "tg" ? 4 : 3;
+            const leaderboard_id = msg.content.includes("tg") ? 4 : 3;
             await axios
                 .get(`https://aoe2.net/api/leaderboard?game=aoe2de&leaderboard_id=${leaderboard_id}&start=1&count=10000&search=[TodEs]`)
                 .then(function (response) {
