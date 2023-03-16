0110dc4bbf7351f153d18d5e85737d6275e2d125
Leandro Pross <leandropross@gmail.com>
2023-03-15 23:30:10 -0300

    Add config directly to index.ts

--- HttpTrigger/index.ts
+++ HttpTrigger/index.ts
@@ -4,9 +4,9 @@ import { AzureFunctionServer, SlashCreator } from "slash-create";
 
 (async () => {
   const creator = new SlashCreator({
-    applicationID: env.BOT_APP_ID,
-    publicKey: env.BOT_APP_PUBLIC_KEY,
-    token: env.BOT_APP_TOKEN,
+    applicationID: "855241297476190228",
+    publicKey: "b9b1b9cbe84fdc1f3a8fdf412cca70f5d30134453c95c48f41386915418fd55a",
+    token: "ODU1MjQxMjk3NDc2MTkwMjI4.YMvnhg.zhaYZ22-xEnFhELTQTl2SWD-wPA",
   });
 
   await creator
@@ -16,7 +16,5 @@ import { AzureFunctionServer, SlashCreator } from "slash-create";
     .registerCommandsIn(join(__dirname, "commands"));
 
   // If a guild have been specified, only sync the command with the guild
-  if (env.COMMANDS_GUILD_ID)
-    creator.syncCommandsIn(env.COMMANDS_GUILD_ID);
-  else creator.syncCommands();
+  creator.syncCommandsIn("690597961775972452");
 })();
